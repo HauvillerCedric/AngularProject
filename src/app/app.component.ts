1156e088c50e0cccb84aae3f2b048cbf69e8f8b7
cedric hauviller <hauviller.cedric@gmail.com>
2024-04-07 11:17:23 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'AngularProject';
+  title = 'Bienvenue sur le site de Cédric !';
 }
